922b877a01608e30dc6b5706d05f67933d41697d
Ignacio Del Busto <gidelbusto@gmail.com>
2020-05-25 01:06:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.map.with_index do |dwarf, i|
+    "#{i + 1}. #{dwarf}"
+    
+  end
 end
 
 def summon_captain_planet# code an argument here
