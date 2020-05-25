e1c5d06dcbffa94834b26a0aaa5431305f486d1b
Ignacio Del Busto <gidelbusto@gmail.com>
2020-05-25 03:02:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,19 +6,26 @@ def roll_call_dwarves(dwarves_names)
   end
 end
 
+
+
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.collect do |call|
     call.capitalize + "!"
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+
+
+def long_planeteer_calls(short_words)
+  short_words.each do |word|
 end
+
+
+
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
-
-def long_planeteer_calls()
